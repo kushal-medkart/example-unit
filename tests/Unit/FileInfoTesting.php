8f66cf904e6d94791558fbc3e092923a655019a5
kushal-medkart <156281670+kushal-medkart@users.noreply.github.com>
2024-05-23 19:19:40 +0530

    validating repository and controller path

--- tests/Unit/FileInfoTesting.php
+++ tests/Unit/FileInfoTesting.php
@@ -2,14 +2,42 @@
 use PHPUnit\Framework\TestCase;
 use App\Repository\DeviceRepository;
 use PHPUnit\Framework\Attributes\DataProvider;
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
+use RegexIterator;
+use Illuminate\Contracts\Foundation\Application;
+
 
 final class FileInfoTesting extends TestCase
 {
-    public function InvalidControllerPath()
+    public function testValidControllerPath()
     {
-        $this->assertTrue(
-            true, "Controller path does not exist in base path: basepath"
+        $controllerPath = 'app/Http/Controllers';
+        $actualControllerPath = realpath(__DIR__.'/../../'.$controllerPath);
+        $iterator = new RecursiveIteratorIterator(
+            new RecursiveDirectoryIterator(__DIR__.'/../../app')
         );
+        $regexIterator = new RegexIterator($iterator, '/^.+Controller\.php$/i', RegexIterator::MATCH);
+
+        foreach ($regexIterator as $file) {
+            $expectedControllerPath = dirname(realpath($file));
+            $this->assertEquals($expectedControllerPath, $actualControllerPath);
+        }
     }
 
+    public function testValidRepositoryPath()
+    {
+        $repositoryPath = 'app/Repository';
+        $actualRepositoryPath = realpath(__DIR__.'/../../'.$repositoryPath);
+
+        $iterator = new RecursiveIteratorIterator(
+            new RecursiveDirectoryIterator(__DIR__.'/../../app')
+        );
+        $regexIterator = new RegexIterator($iterator, '/^.+Repository\.php$/i', RegexIterator::MATCH);
+
+        foreach ($regexIterator as $file) {
+            $expectedRepositoryPath = dirname(realpath($file));
+            $this->assertEquals($expectedRepositoryPath, $actualRepositoryPath);
+        }
+    }
 }
